e536356dbd8757594ccfb08f5c52172390b8d527
c14180066 <c14180066@john.petra.ac.id>
2020-04-15 08:52:25 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Niggee :))))';
+  itembuah = [{
+    nama: "kelengkeng",
+    harga : 10000
+  },{
+    nama: 'Jeruk',
+    harga : 5000
+  }];
+
+  itemArr = ['aa','bb','cc'];
+
+  show: boolean = false;
+
+  pesan = '';
+  klikButton(){
+    this.pesan = 'Tombol Ditekan';
+  }
 }
